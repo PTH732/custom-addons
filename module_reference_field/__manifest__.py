fb4ecf668e416941eca8da2425741405c0d8fb76
PTH732 <phanhieu07032002@gmail.com>
2025-05-20 14:11:13 +0700

    Đổi tên thư mục module thành đúng

---
+++ module_reference_field/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "module-reference",
+    'summary': """Short (1 phrase/line) summary of the module's purpose, used as subtitle on modules listing or apps.openerp.com""",
+    'description': """
+        Long description of module's purpose
+    """,
+    'author': "PTH",
+    'category': 'Uncategorized',
+    'license': 'OEEL-1',
+    'version': '0.1',
+    'data': [
+        'views/root_menu.xml',
+        'views/people_views.xml',
+    ],
+    'depends': ['base'],
+    'installable': True,
+    'application': True,
+}
